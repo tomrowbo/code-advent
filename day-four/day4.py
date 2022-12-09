13ffcd405c18efccd52168e58050d0c29b674030
Tom Rowbotham <tom.rowbotham@gousto.co.uk>
2022-12-09 14:43:08 +0000

    Day 4

---
+++ day-four/day4.py
@@ -0,0 +1,24 @@
+def is_contained_in_range(pair1, pair2):
+    if (pair2[0] <= pair1[0] <= pair2[1] and pair2[0] <= pair1[1] <= pair2[1]) or (
+            pair1[0] <= pair2[0] <= pair1[1] and pair1[0] <= pair2[1] <= pair1[1]):
+        return True
+    return False
+
+
+with open('day4.txt') as f:
+    lines = f.readlines()
+    count = 0
+    for line in lines:
+        pair = line.strip()
+        #     count += solve_riddle(pair)
+        # #
+        # print(count)
+        shift = pair.split(",")
+        shifts = [shift[0].split("-"), shift[1].split("-")]
+        pair1 = [int(shifts[0][0]),int(shifts[0][1])]
+        pair2 = [int(shifts[1][0]), int(shifts[1][1])]
+        containedInRange = (is_contained_in_range(pair1, pair2))
+        if containedInRange:
+            count += 1
+    print(count)
+
